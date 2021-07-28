eeb45d0712a23bfb9f11fa2e15a17f309f506671
Vedran Bidin <vedran@bidin.me>
2021-07-28 12:39:10 +0200

    Update ERC-165 parameter names

--- src/interfaces/standards/IERC165.sol
+++ src/interfaces/standards/IERC165.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 /// @title Interface of the ERC-165 standard
 /// @dev See https://eips.ethereum.org/EIPS/eip-165
 interface IERC165 {
-    /// @notice Checks if this contract implements the interface defined by `id`.
+    /// @notice Checks if this contract implements the interface defined by `interfaceId`.
     /// @dev See https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified
     /// This function call must use less than 30,000 gas.
-    /// @param id The interface identifier.
-    /// @return Whether the contracts implements the interface defined by `id`.
-    function supportsInterface(bytes4 id) external view returns (bool);
+    /// @param interfaceId The interface identifier.
+    /// @return Whether the contracts implements the interface defined by `interfaceId`.
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
