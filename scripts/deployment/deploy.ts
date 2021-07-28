d44dd4ccf57002cb002890912b434aeaeecfe0d2
Vedran Bidin <vedran@bidin.me>
2021-07-29 00:12:27 +0200

    Remove gas limit and gas price

--- scripts/deployment/deploy.ts
+++ scripts/deployment/deploy.ts
@@ -20,8 +20,6 @@ async function main() {
   const [providerUrl, privateKey, ethGasStationUrl, etherscanKey] = validate();
   const contractAddresses: string[] = await deployAndVerify(
     getSigner(privateKey, getProvider(providerUrl)),
-    await getGasLimit(providerUrl),
-    await getGasPrice(ethGasStationUrl),
     etherscanKey
   );
   await writeFile(options.outputFile, JSON.stringify(contractAddresses));
@@ -56,44 +54,21 @@ function getSigner(privateKey: string, provider: providers.Provider): Signer {
   return new Wallet(privateKey, provider);
 }
 
-async function getGasLimit(providerUrl: string): Promise<BigNumber> {
-  //provider: Provider = getProvider(providerUrl);
-  //const blockNumber: number = await provider.getBlockNumber();
-  //const block: Block = await provider.getBlock(blockNumber);
-  return BigNumber.from(4 * 10 ** 6);
-}
-
-async function getGasPrice(ethGasStationUrl: string): Promise<BigNumber> {
-  //const request = await get(ethGasStationUrl);
-  //return BigNumber.from(request.data["average"])
-  //  .div(10)
-  //  .mul(10 ** 9);
-  return BigNumber.from(2 * 10 ** 9);
-}
-
 async function deployAndVerify(
   signer: Signer,
-  gasLimit: BigNumber,
-  gasPrice: BigNumber,
   etherscanKey: string
 ): Promise<string[]> {
   const contractAddresses: string[] = [];
   for (let i = 0; i < options.contractNames.length; i++) {
-    console.log(
-      `deploying ${options.contractNames[i]}, gasLimit: ${gasLimit}, gasPrice: ${gasPrice}`
-    );
+    console.log(`deploying ${options.contractNames[i]}`);
     const contract = await deploy(
       options.contractNames[i],
       options.constructorArguments[i],
-      signer,
-      gasLimit,
-      gasPrice
+      signer
     );
     console.log(`deployed ${options.contractNames[i]}`);
     contractAddresses.push(contract.address);
-    console.log("before linking:", options.constructorArguments);
     link(i, contract.address);
-    console.log("after linking:", options.constructorArguments);
     await verify(contract.address, etherscanKey);
   }
 
@@ -103,13 +78,10 @@ async function deployAndVerify(
 async function deploy(
   contractName: string,
   constructorArguments: any[],
-  signer: Signer,
-  gasLimit: BigNumber,
-  gasPrice: BigNumber
+  signer: Signer
 ): Promise<Contract> {
   let factory: ContractFactory = await ethers.getContractFactory(contractName);
   factory = factory.connect(signer);
-  constructorArguments.push({ gasLimit, gasPrice });
   const contract: Contract = await factory.deploy(...constructorArguments);
   return await contract.deployed();
 }
