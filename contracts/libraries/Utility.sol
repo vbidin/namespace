51540ff8700a1be9e99518e868415b35228ecf18
Vedran Bidin <vedran@bidin.me>
2021-07-30 22:17:27 +0200

    Add utility library

---
+++ contracts/libraries/Utility.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Utility {
+    function isZero(address address_) internal pure returns (bool) {
+        return address_ == address(0);
+    }
+
+    function isEmpty(string calldata string_) internal pure returns (bool) {
+        return bytes(string_).length == 0;
+    }
+
+    function containsPeriods(string calldata string_)
+        internal
+        pure
+        returns (bool)
+    {
+        // iterate through bytes(s) and compare to '.'
+        // can there be any false positives with utf-8?
+        // TODO
+        return false;
+    }
+}
