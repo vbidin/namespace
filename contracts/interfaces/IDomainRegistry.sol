5873140f4444c97c35aaaa40a95bed830be873f3
Vedran Bidin <vedran@bidin.me>
2021-07-31 14:53:32 +0200

    Remove standards folder

--- contracts/interfaces/IDomainRegistry.sol
+++ contracts/interfaces/IDomainRegistry.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./standards/IERC721.sol";
+import "./IERC721.sol";
 
 /// @title Interface of a registry of domain ownerships
 /// @notice Complies with the ERC-721 Non-Fungible Token Standard.
