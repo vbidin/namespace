79023bfda797142ac15e06c7d7b628386ddf70ab
Vedran Bidin <vedran@bidin.me>
2021-08-10 23:20:23 +0200

    Add domain lists

---
+++ scripts/deployment/domains.ts
@@ -0,0 +1,7 @@
+// List of effective top-level domains, extracted from: https://publicsuffix.org/list/
+// These domains will be preemptively registered as public domains.
+export const EffectiveTopLevelDomains = ["co.uk", "..."];
+
+// List of 'X' most popular domains, extracted from: https://www.alexa.com/topsites (or maybe somewhere else...)
+// These domains will be preemptively registered as private domains.
+export const PopularDomains = ["google.com", "..."];
