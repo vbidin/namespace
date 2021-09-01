4c69d6d7e9926c572512d8d5303bef6598f561d2
Vedran Bidin <vedran@bidin.me>
2021-09-01 22:34:20 +0200

    Update deployment options documentation

--- scripts/deployment/options.ts
+++ scripts/deployment/options.ts
@@ -1,13 +1,16 @@
 import hre from "hardhat";
 import { Contracts } from "../enums/contracts";
 
-// location where addresses of all deployed contracts will be stored
+// The file where contract addresses are stored during deployment.
 export const DeploymentOutputFile =
   hre.config.paths.artifacts + "/deployment.json";
 
-// contract names and their constructor arguments
-// contracts will be deployed in insertion order
-// references to other contracts will be replaced with deployment addresses
+/* 
+  A map that links contracts with their constructor arguments.
+  All contracts are deployed in the same order as defined here.
+  Constructor arguments can be references to other contracts,
+  and will be replaced with actual contract addresses during deployment.
+*/
 export const ConstructorArguments: Map<string, any> = new Map([
   [Contracts.DomainRegistry, { domainDuration: 31536000 }],
   [Contracts.RecordStorage, { domainRegistry: Contracts.DomainRegistry }],
