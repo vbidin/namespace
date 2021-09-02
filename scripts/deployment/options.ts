7b7fbe59b5a98b4305790942acb57ea6c9aede48
Vedran Bidin <vedran@bidin.me>
2021-09-02 23:33:41 +0200

    Add libraries to deployment options

--- scripts/deployment/options.ts
+++ scripts/deployment/options.ts
@@ -1,15 +1,18 @@
 import hre from "hardhat";
 import { Contracts } from "../enums/contracts";
 
-// The file where contract addresses are stored during deployment.
+// The file where addresses of deployed contracts are stored during deployment.
 export const DeploymentOutputFile =
   hre.config.paths.artifacts + "/deployment.json";
+
+// List of libraries that will be deployed and linked with contracts before they are deployed.
+export const Libraries: string[] = [];
 
 /* 
   A map that links contracts with their constructor arguments.
   All contracts are deployed in the same order as defined here.
   Constructor arguments can be references to other contracts,
-  and will be replaced with actual contract addresses during deployment.
+  and will be replaced with deployment addresses during deployment.
 */
 export const ConstructorArguments: Map<string, any> = new Map([
   [Contracts.DomainRegistry, { domainDuration: 31536000 }],
